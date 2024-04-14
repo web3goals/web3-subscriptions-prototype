626ddb3b32cc3a43b7441ae23f12a4d5bbaa7958
kiv1n <aloha@kiv1n.ru>
2024-04-14 10:15:01 +0300

    Improve deploy contract scripts

--- contracts/scripts/deploy-contracts.ts
+++ contracts/scripts/deploy-contracts.ts
@@ -1,17 +1,30 @@
 import { ethers } from "hardhat";
+import hre from "hardhat";
 
-// Etherlink data
-export const CONTRACTS = {
-  entryPoint: "0x539dA825856778B593a55aC4E8A0Ec1441f18e78",
-  accountFactory: "0xBBae3088AaF60c44Fb932ba82fd0b3dbb2d67C6F",
-  paymaster: "0x57d1469c53Bb259Dc876A274ADd329Eb703Ab286",
-  product: "0x4F3681bA990a80e7f5F1BdDDADF7c0D864f10342",
-  usdToken: "0xe720443310986E173af339fA366A30aa0A1Ea5b2",
+export const CONTRACTS: {
+  [key: string]: {
+    entryPoint: `0x${string}` | undefined;
+    accountFactory: `0x${string}` | undefined;
+    paymaster: `0x${string}` | undefined;
+    product: `0x${string}` | undefined;
+    usdToken: `0x${string}` | undefined;
+  };
+} = {
+  etherlinkTestnet: {
+    entryPoint: "0x539dA825856778B593a55aC4E8A0Ec1441f18e78",
+    accountFactory: "0xBBae3088AaF60c44Fb932ba82fd0b3dbb2d67C6F",
+    paymaster: "0x57d1469c53Bb259Dc876A274ADd329Eb703Ab286",
+    product: "0x86916184b00b26dceaF63a2cD6c9095314f6e055",
+    usdToken: "0xe720443310986E173af339fA366A30aa0A1Ea5b2",
+  },
 };
 
 async function main() {
   console.log("👟 Start script 'deploy-contracts'");
-  if (!CONTRACTS.entryPoint) {
+
+  const network = hre.network.name;
+
+  if (!CONTRACTS[network].entryPoint) {
     const contractFactory = await ethers.getContractFactory("CustomEntryPoint");
     const contract = await contractFactory.deploy();
     await contract.waitForDeployment();
@@ -19,7 +32,8 @@ async function main() {
       `Contract 'EntryPoint' deployed to: ${await contract.getAddress()}`
     );
   }
-  if (!CONTRACTS.accountFactory) {
+
+  if (!CONTRACTS[network].accountFactory) {
     const contractFactory = await ethers.getContractFactory("AccountFactory");
     const contract = await contractFactory.deploy();
     await contract.waitForDeployment();
@@ -27,7 +41,8 @@ async function main() {
       `Contract 'AccountFactory' deployed to: ${await contract.getAddress()}`
     );
   }
-  if (!CONTRACTS.paymaster) {
+
+  if (!CONTRACTS[network].paymaster) {
     const contractFactory = await ethers.getContractFactory("Paymaster");
     const contract = await contractFactory.deploy();
     await contract.waitForDeployment();
@@ -35,7 +50,8 @@ async function main() {
       `Contract 'Paymaster' deployed to: ${await contract.getAddress()}`
     );
   }
-  if (!CONTRACTS.product) {
+
+  if (!CONTRACTS[network].product) {
     const contractFactory = await ethers.getContractFactory("Product");
     const contract = await contractFactory.deploy();
     await contract.waitForDeployment();
@@ -43,7 +59,8 @@ async function main() {
       `Contract 'Product' deployed to: ${await contract.getAddress()}`
     );
   }
-  if (!CONTRACTS.usdToken) {
+
+  if (!CONTRACTS[network].usdToken) {
     const contractFactory = await ethers.getContractFactory("USDToken");
     const contract = await contractFactory.deploy();
     await contract.waitForDeployment();
